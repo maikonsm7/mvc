4cf2ded4889f64aa9951c6f521076c80c9567d15
Maikon Monteiro <maikonsm7@gmail.com>
2024-06-09 01:11:55 -0400

    create project

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+app.use(express.static('public'))
+
+//configuração handlebars
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res)=>{
-    res.send('Bem vindo!')
+    res.redirect('/home')
+})
+
+app.get('/home', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/sobre', (req, res)=>{
+    res.render('sobre')
 })
 
 app.listen(port, ()=>{
